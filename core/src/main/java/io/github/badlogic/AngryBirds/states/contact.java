ebd5e60b6c316f987a66282d53bbebe7e9448a99
GunikaSingh <gunika505@gmail.com>
2024-11-25 23:08:39 +0530

    velocity dependent hit

--- core/src/main/java/io/github/badlogic/AngryBirds/states/contact.java
+++ core/src/main/java/io/github/badlogic/AngryBirds/states/contact.java
@@ -18,26 +18,34 @@ public class contact implements ContactListener {
 
         // Check for specific collisions and take actions
         if (userDataA instanceof Bird && userDataB instanceof Block) {
-            ((Block) userDataB).health-=1;
-            if (((Block) userDataB).health==0){
-                ((Block) userDataB).dispose(level);
+            Block block=(Block) userDataB;
+            Bird bird=(Bird) userDataA;
+            block.health-= (int) (bird.body.getLinearVelocity().len()/110)+1;
+            if (block.health<=0){
+                block.dispose(level);
             }
         } else if (userDataA instanceof Block && userDataB instanceof Bird) {
-            ((Block) userDataA).health-=1;
-            if (((Block) userDataA).health==0){
-                ((Block) userDataA).dispose(level);
+            Block block=(Block) userDataA;
+            Bird bird=(Bird) userDataB;
+            block.health-= (int) (bird.body.getLinearVelocity().len()/110)+1;
+            if (block.health<=0){
+                block.dispose(level);
             }
         }
         else if (userDataA instanceof Bird && userDataB instanceof Pig) {
-            ((Pig) userDataB).health-=1;
-            if (((Pig) userDataB).health==0){
-                ((Pig) userDataB).dispose(level);
+            Pig pig=(Pig) userDataB;
+            Bird bird=(Bird) userDataA;
+            pig.health-= (int) (bird.body.getLinearVelocity().len()/110)+1;
+            if (pig.health<=0){
+                pig.dispose(level);
             }
-        } else if (userDataA instanceof Pig && userDataB instanceof Bird) {
-            ((Pig) userDataA).health-=1;
-            System.out.println(((Pig) userDataA).health);
-            if (((Pig) userDataA).health==0){
-                ((Pig) userDataA).dispose(level);
+        }
+        else if (userDataB instanceof Bird && userDataA instanceof Pig) {
+            Pig pig=(Pig) userDataA;
+            Bird bird=(Bird) userDataB;
+            pig.health-= (int) (bird.body.getLinearVelocity().len()/110)+1;
+            if (pig.health<=0){
+                pig.dispose(level);
             }
         }
 
